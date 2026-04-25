42c1e85a08435c08dd40f3efd009e95fe2980149
KarthikRP424 <karthukarthik242@gmail.com>
2026-04-25 19:44:13 +0530

    Add files via upload

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,17 @@
-# Check whether a number is Palindrome or not
 def palindrome(n):
-    if n == reversed(n):
-        return palindrome
-    else:
-        return not palindrome 
+    if n == 0:
+        return 0
+    original = n
+    reverse = 0
     
-n = int(input("enter the numbers"))
+    while n > 0:
+        digit = n %10
+        reverse = reverse * 10 + digit
+        n = n//10
+    if original == reverse:
+        print("the number is palindrome")
+    else:
+        print("not a palindrome")
+
+n = int(input("enter the number"))
 print(palindrome(n))
